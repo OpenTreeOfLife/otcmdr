e53b875263e0c89491ebcb1ac43a4c9bc1d86315
Joseph W. Brown <phylo.jwb@gmail.com>
2015-10-20 14:38:07 -0400

    doh forgot conf

---
+++ conf.py
@@ -0,0 +1,16 @@
+"""
+These shouldn't really ever change.
+"""
+
+# java settings. need lots 'o memory for big analyses'
+javapre = "java -XX:+UseConcMarkSweepGC -Xmx32g -server -jar"
+
+# where is treemachine?
+treemloc = "/home/josephwb/Work/OToL/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+
+# where is the taxonomy (ott)?
+otloc = "/home/josephwb/Work/OToL/ott/ott2.9draft12/"
+
+# where is otcetera (specifically the supertree directory)?
+otceteraloc = "/home/josephwb/Work/OToL/otcetera/supertree/"
+
